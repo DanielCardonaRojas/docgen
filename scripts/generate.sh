95fba307f868a20753d3804136986c8bccfd6dc2
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2024-05-07 11:06:56 -0500

    Default file name based on spec name

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -13,28 +13,44 @@ BASE_DIR="$DOC_CONTEXT/$DOC_TYPE"
 
 # CHOOSE THE CONFIG TO USE
 
-CONFIG_FILE=$(ls $BASE_DIR/configs | tr '\n' ' ' | xargs -L 1 gum choose --header "Pick a config")
+CONFIG_FILE=$(ls $BASE_DIR/configs | head -n 1)
+
+CONFIG_FILE_COUNT=$(ls $BASE_DIR/configs | wc -l | tr -s ' ')
+
+if [ "$CONFIG_FILE_COUNT" -gt 1 ]; then
+    CONFIG_FILE=$(ls $BASE_DIR/configs | tr '\n' ' ' | xargs -L 1 gum choose --header "Pick a config")
+fi
 
 CURRENT_DIR="$(pwd)"
-echo "current working directory: $CURRENT_DIR"
 cd "$BASE_DIR"
 
 # IS THERE A SCRIPT THAAT CAN CALCULATE THE OUTPUT FILE NAME?
 
-FILENAME=""
-if [ -f "$BASE_DIR/scripts/file_name.sh" ]; then
-    FILENAME=$(bash "$BASE_DIR/scripts/file_name.sh")
+SPEC_NAME=$(basename "$BASE_DIR")
+FILENAME="${SPEC_NAME%Spec}"
+
+
+
+if [ -f "$BASE_DIR/scripts/doc_gen_file_name.sh" ]; then
+    FILENAME=$(bash "$BASE_DIR/scripts/doc_gen_file_name.sh")
 else
     echo "No automatic output file name for template"
 fi
 
+# echo "Spec: $SPEC_NAME file: $FILENAME current dir: $CURRENT_DIR"
 
 # WRITE TO A SPECIFIC FILE NAME OR TO STDOUT
 
 if [ -n "$FILENAME" ]; then
-    echo "writting to: $CURRENT_DIR/$FILENAME"
     gomplate --config="$BASE_DIR/configs/$CONFIG_FILE" -o - > "$CURRENT_DIR/$FILENAME"
 else
     gomplate --config="$BASE_DIR/configs/$CONFIG_FILE" -o -
 fi
 
+
+# CLEANUP
+if [ -f "$BASE_DIR/scripts/doc_gen_cleanup.sh" ]; then
+    bash "$BASE_DIR/scripts/doc_gen_cleanup.sh"
+else
+    echo "No cleanup required"
+fi
