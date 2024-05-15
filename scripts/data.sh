17fa5cc814e0fbfffe0982482d9b74673550d63a
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2024-05-14 23:29:46 -0500

    Update so that new custom data entries are put in ~/.config/docen/SPEC/data
    - Symlinks back to ~/.local/share/docgen/data

--- scripts/data.sh
+++ scripts/data.sh
@@ -3,24 +3,42 @@
 # This script creates new data file to populate a template.
 
 
-DOC_CONTEXT="${DOCGEN_PATH:-$HOME/.config/docgen}"
+DOC_GEN_SPEC_DIR="${DOCGEN_SPEC_PATH:-$HOME/.local/share/docgen}"
+DOC_GEN_DIR="${DOCGEN_PATH:-$HOME/.config/docgen}"
 
 # PICK THE DOCUMENT YOU ARE TRYING TO GENERATE E.G INVOICE, CV, REPORT
 # THIS WILL DEPEND ON THE FOLDER STRUCTURE CREATED UNDER THE CONFIGURATION DIRECTORY
 
-DOC_TYPE=$(ls -d $DOC_CONTEXT/*/ | xargs -L 1 basename | gum choose --header "Pick type of document")
-BASE_DIR="$DOC_CONTEXT/$DOC_TYPE"
+if [ ! -d "$DOC_GEN_SPEC_DIR" ]; then
+    echo "No available specs"
+    exit 1
+fi
 
-# Duplicate the examples file
-EXAMPLE_FILE_PATH=$(find $BASE_DIR/data -type f -name "example.*")
+DOC_TYPE=$(ls -d $DOC_GEN_SPEC_DIR/*/ | xargs -L 1 basename | gum choose --header "Pick type of document")
+SPEC_DIR="$DOC_GEN_SPEC_DIR/$DOC_TYPE/data"
+MIRROR_DIR="$DOC_GEN_DIR/$DOC_TYPE/data"
+
+# Create a mirror of the data folder of the spec in ~/.config/docgen/<SPEC_NAME>/data
+
+if [ ! -d "$MIRROR_DIR" ]; then
+    mkdir -p "$MIRROR_DIR"
+fi
+
+# Duplicate the examples file and symlink
+
+EXAMPLE_FILE_PATH=$(find $SPEC_DIR -type f -name "example.*")
 EXAMPLE_FILE=$(basename "$EXAMPLE_FILE_PATH")
 EXAMPLE_FILE_EXTENSION="${EXAMPLE_FILE##*.}"
+FILE_NAME="default.$EXAMPLE_FILE_EXTENSION"
 
-if [ ! -f "$BASE_DIR/data/default.$EXAMPLE_FILE_EXTENSION" ]; then
-    cp "$EXAMPLE_FILE_PATH" "$BASE_DIR/data/default.$EXAMPLE_FILE_EXTENSION"
+if [ ! -f "$MIRROR_DIR/default.$EXAMPLE_FILE_EXTENSION" ]; then
+    cp "$EXAMPLE_FILE_PATH" "$MIRROR_DIR/$FILE_NAME"
+    ln -s "$MIRROR_DIR/$FILE_NAME" "$SPEC_DIR/$FILE_NAME"
 else
-    FILE_NAME=$(gum input --placeholder "Please provide a name for data file")
-    cp "$EXAMPLE_FILE_PATH" "$BASE_DIR/data/$FILE_NAME.$EXAMPLE_FILE_EXTENSION"
+    NAME=$(gum input --placeholder "Please provide a name for data file")
+    FILE_NAME="$NAME.$EXAMPLE_FILE_EXTENSION"
+    cp "$EXAMPLE_FILE_PATH" "$MIRROR_DIR/$FILE_NAME"
+    ln -s "$MIRROR_DIR/$FILE_NAME" "$SPEC_DIR/$FILE_NAME"
 fi
 
 
